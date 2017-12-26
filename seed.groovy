02c0eb30f2fc0d8a75846027dbcb2cb77e74d6b4
Peder Korsveien <peder.korsveien@nav.no>
2017-12-26 14:47:23 +0100

    Change NodeJS version

--- seed.groovy
+++ seed.groovy
@@ -1,6 +1,6 @@
 job('sparefant-fe-master') {
     wrappers {
-        nodejs('NodeJS 1.2.4')
+        nodejs('NodeJS 9.3.0')
     }
     scm {
         github(ownerAndProject = 'pederpus/sparefant-fe', branch = 'master',  protocol = 'git')
